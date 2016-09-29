07d23a245333fdeb144833bd19eb108da4efe7aa
Rahajeng <rahajeng_violita_23rpl@student.smktelkom-mlg.sch.id>
2016-09-29 15:25:09 +0700

    First Commit
    Menambahkan textView, imageView, editText, Spinner, Button, Radio Goup (dengan 2 RadioButton), CheckBox
    
    Second Commit
    
    Mengganti gambar, mengatur textView, editText, Spinner, dll
    Menambahkan program pengeksekusian (memunculkan editText nama, tanggal, dan bulan)
    Menambah build.grande design
    
    Third Commit
    Mengatur ulang tampilan
    
    Menambahkan setText nama.
    Menambahkan setText Spinner Trip.
    Menambahkan radioButton (ada 2 pilihan, open trip dan private) sebagai pilihan paket.
    Menambahkan jadwal keberangkatan.
    Menambahkan set error, apabila tanggal dan bulan tidak diisi akan muncul error.
    Menambahkan checkbox sebagai ektra.

--- app/src/main/java/id/sch/smktelkom_mlg/tugas01/xiirpl4027/formtrip/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/tugas01/xiirpl4027/formtrip/MainActivity.java
@@ -4,15 +4,19 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.Spinner;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     EditText eNama, tgl, bln;
     Button b;
+    RadioButton rOT, rP;
     TextView h;
     Spinner t;
+    CheckBox sa, tu, ti, em;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,11 +24,24 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         eNama = (EditText) findViewById(R.id.editText);
+
         b = (Button) findViewById(R.id.button);
+
         h = (TextView) findViewById(R.id.textViewH);
+
         t = (Spinner) findViewById(R.id.spinner);
+
         tgl = (EditText) findViewById(R.id.editText2);
         bln = (EditText) findViewById(R.id.editText3);
+
+        rOT = (RadioButton) findViewById(R.id.radioButton);
+        rP = (RadioButton) findViewById(R.id.radioButton2);
+
+        sa = (CheckBox) findViewById(R.id.checkBox);
+        tu = (CheckBox) findViewById(R.id.checkBox2);
+        ti = (CheckBox) findViewById(R.id.checkBox3);
+        em = (CheckBox) findViewById(R.id.checkBox4);
+
         b.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
                 doProcess();
@@ -32,15 +49,34 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+
     private void doProcess() {
         String nama = eNama.getText().toString();
         String tanggal = tgl.getText().toString();
         String bulan = bln.getText().toString();
+        String trip = t.getSelectedItem().toString();
+
+        String hasil = null;
+
+        String has = "";
+        int startlen = has.length();
+        if (sa.isChecked()) has += sa.getText() + "\n";
+        if (tu.isChecked()) has += tu.getText() + "\n";
+        if (ti.isChecked()) has += ti.getText() + "\n";
+        if (em.isChecked()) has += em.getText() + "\n";
+
+        if (has.length() == startlen) has += "Tidak ada pada Pilihan";
+        if (rOT.isChecked()) {
+            hasil = rOT.getText().toString();
+        } else if (rP.isChecked()) {
+            hasil = rP.getText().toString();
+        }
 
         if (isValid()) {
             h.setText("Pemesanan atas nama " + nama +
-                    "\nAkan melakukan perjalanan " +
+                    "\nAkan melakukan perjalanan " + hasil + " " + trip +
                     " pada tanggal " + tanggal + " bulan " + bulan
+                    + "\n\nExtra : \n" + has
             );
         }
     }
@@ -54,25 +90,29 @@ public class MainActivity extends AppCompatActivity {
 
         if (nama.isEmpty()) {
             eNama.setError("Nama belum diisi");
+            valid = false;
         } else {
             eNama.setError(null);
         }
 
         if (tanggal.isEmpty()) {
             tgl.setError("Tanggal belum diisi");
+            valid = false;
         } else if (tanggal.length() != 2) {
             tgl.setError("Format tangal tidak sesuai");
+            valid = false;
         } else {
             tgl.setError(null);
         }
         if (bulan.isEmpty()) {
             bln.setError("Bulan belum diisi");
+            valid = false;
         } else if (bulan.length() != 2) {
             bln.setError("Format bulan tidak sesuai");
+            valid = false;
         } else {
             bln.setError(null);
         }
-        return false;
+        return valid;
     }
-});
-        }
+}
